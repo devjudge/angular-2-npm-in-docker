83db809d928834ffafccd0260ffc231a635026bb
Rajat Tyagi <40917672+tyagirajat200@users.noreply.github.com>
2021-06-15 11:02:18 +0530

    Update chrome-test-setup.sh

--- chrome-test-setup.sh
+++ chrome-test-setup.sh
@@ -1,13 +1,10 @@
+echo "Updating..."
+sudo apt-get update
+
 echo "Installing Chrome for Selenium.."
 cd /tmp
 wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_80.0.3987.116-1_amd64.deb
 sudo apt install --assume-yes ./google-chrome-stable_80.0.3987.116-1_amd64.deb
-
-echo "Updating..."
-sudo apt-get update
-
-echo "Installing libnss3-dev..."
-sudo apt install libnss3-dev
 
 echo "Installing chromedriver for Selenium..."
 cd /tmp
